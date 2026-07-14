40589437f23215e2816723a67507872fc6230441
Nexus <dikatenlucky48@gmail.com>
2026-07-14 21:47:38 +0700

    eight commit 2.0

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -395,7 +395,7 @@
             <div class="max-w-7xl mx-auto px-4 text-center">
                 <h3 class="text-2xl font-extrabold mb-2 tracking-wide">ANI<span class="text-yellow-400">merch</span>
                 </h3>
-                <p class="text-slate-400 text-sm">&copy; 2024 Kyou Clone Project. Tugas Teknik Informatika.</p>
+                <p class="text-slate-400 text-sm">&copy; 2026 ANImerch. All rights reserved.</p>
             </div>
         </footer>
     </div>
